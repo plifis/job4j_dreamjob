f44420e7e178e46e2a4303daeefed675ae1c30b1
plifis <nikich85@inbox.ru>
2021-04-27 09:48:38 +0300

    3. Расширить Store для пользователя. [#492944]

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -2,18 +2,20 @@ package ru.job4j.dream.store;
 
 import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
+import ru.job4j.dream.model.User;
 
 import java.util.Collection;
 
 public interface Store {
     Collection<Post> findAllPosts();
-
     Collection<Candidate> findAllCandidates();
+    Collection<User> findAllUsers();
 
     void save(Post post);
-
     void save(Candidate candidate);
+    void save(User user);
 
     Post findPostById(int id);
     Candidate findCandidateById(int id);
+    User findUserById(int id);
 }
