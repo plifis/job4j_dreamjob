4f37f735e531d69912d8eccc732384ca7c85e6fb
plifis <nikich85@inbox.ru>
2021-05-04 17:48:25 +0300

    4.1. JSON формат. [#492915]

--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -1,11 +1,16 @@
 package ru.job4j.dream.servlet;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GreetingServlet extends HttpServlet {
 
@@ -18,4 +23,19 @@ public class GreetingServlet extends HttpServlet {
         writer.println("Nice to meet you, " + name);
         writer.flush();
     }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("UTF-8");
+        List<JSONObject> list = new ArrayList<>();
+        String data;
+        while ((data = req.getReader().readLine()) != null) {
+                list.add(new JSONObject(data));
+        }
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.println(list.toString());
+        writer.flush();
+
+    }
+
 }
